316f2cbd79dbf92a424a2b20363c896caf40e79a
ritchey0713 <ritchey0713@gmail.com>
2020-10-19 11:01:00 -0400

    add cors file logic

---
+++ config/initializers/cors.rb
@@ -0,0 +1,14 @@
+Rails.application.config.middleware.insert_before 0, Rack::Cors do 
+  allow do 
+    origin "http://localhost:3001"
+    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
+    credentials: true
+  end 
+
+  # prod domain placeholder
+  # allow do 
+  #   origin "http://localhost:3001"
+  #   resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
+  #   credentials: true
+  # end 
+end 
