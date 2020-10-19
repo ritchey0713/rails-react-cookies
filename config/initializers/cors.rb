e94e3ad0d5d128da4eecd4b4581b6bf9102fd5fb
ritchey0713 <ritchey0713@gmail.com>
2020-10-19 11:11:42 -0400

    add users

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,13 +1,13 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do 
   allow do 
-    origin "http://localhost:3001"
+    origins "http://localhost:3001"
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
     credentials: true
   end 
 
   # prod domain placeholder
   # allow do 
-  #   origin "http://localhost:3001"
+  #   origins "http://localhost:3001"
   #   resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
   #   credentials: true
   # end 
